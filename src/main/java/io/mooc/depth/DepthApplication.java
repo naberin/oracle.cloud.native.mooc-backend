34cf328bbfb7134824a05084600ef2b2359e72ae
naberin <aberinnj@gmail.com>
2021-12-21 00:00:06 -0600

    Added initial files

---
+++ src/main/java/io/mooc/depth/DepthApplication.java
@@ -0,0 +1,13 @@
+package io.mooc.depth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepthApplication.class, args);
+    }
+
+}
