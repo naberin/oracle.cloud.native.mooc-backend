d8ea6ea336e518737444b735aa432a3ab48ad308
naberin <aberinnj@gmail.com>
2021-12-22 21:27:46 -0600

    Added DB design v1.0.0

---
+++ scripts/reset.sql
@@ -0,0 +1,10 @@
+-- author: Norman Aberin
+-- version: v1.0.0
+--
+
+DROP TABLE OC_CATEGORIES cascade constraints;
+DROP TABLE OC_COURSES cascade constraints;
+DROP TABLE OC_LESSON_SECTIONS cascade constraints;
+DROP TABLE OC_LESSONS cascade constraints;
+DROP TABLE OC_TAGS cascade constraints;
+DROP TABLE OC_COURSE_TAGS cascade constraints;
