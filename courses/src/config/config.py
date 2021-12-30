9c0b1e46a1804bd2bc7381b2b82b375baf2c0222
naberin <aberinnj@gmail.com>
2021-12-30 00:48:37 -0600

    Added config and DB constants

---
+++ courses/src/config/config.py
@@ -0,0 +1,8 @@
+import os
+
+from constants import EnvironmentConstants
+
+DB_DSN = os.getenv(EnvironmentConstants.DB_DSN)
+DB_USER = os.getenv(EnvironmentConstants.DB_USER)
+DB_PWD = os.getenv(EnvironmentConstants.DB_PWD)
+
--- courses/src/config/constants.py
+++ courses/src/config/constants.py
@@ -1,5 +1,6 @@
 
 class ErrorConstants:
+    """Common Errors"""
     DEFAULT_NOT_FOUND_ERROR = "Not Found"
     DEFAULT_NOT_FOUND_DETAIL = "Resource was not found"
 
@@ -7,4 +8,16 @@ class ErrorConstants:
     DEFAULT_INTERNAL_DETAIL = "Request cannot be currently completed. Please contact support or your administrator."
 
 
+class EnvironmentConstants:
+    """environment variables to read from"""
+    DB_DSN = "DPT_DB_DSN"
+    DB_USER = "DPT_DB_USER"
+    DB_PWD = "DPT_DB_PWD"
 
+
+class DBConfigurationConstants:
+    DB_POOL_MIN = 15
+    DB_POOL_MAX = 15
+    DB_POOL_INC = 0
+    DB_POOL_THR = True
+    DB_POOL_ENC = "UTF-8"
