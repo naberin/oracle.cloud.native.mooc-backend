998eb699c7e5b25ee8ab554f02aa1ad83516c9dd
naberin <aberinnj@gmail.com>
2021-12-30 01:54:38 -0600

    Registered Courses

--- courses/app.py
+++ courses/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from src.controllers.HealthController import health
 from src.controllers.CategoryController import category
+from src.controllers.CourseController import courses
 
 from src.resources.Pool import Pool
 
@@ -11,6 +12,7 @@ pool = Pool().get()
 app = Flask(__name__)
 app.register_blueprint(category)
 app.register_blueprint(health)
+app.register_blueprint(courses)
 
 
 if __name__ == "__main__":
